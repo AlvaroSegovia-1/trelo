317fce06171fd92f66d1e9438733a78ed93b0451
Alvagit25 <alvarodesegovia@gmail.com>
2022-04-20 18:55:04 +0200

    añadir context

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,36 @@
-import { Typography } from '@mui/material'
 import type { NextPage } from 'next'
+import { CardContent, CardHeader, Card, Grid, Typography } from '@mui/material'
+
 import { Layout } from '../components/layouts'
 
 
 const HomePage: NextPage = () => {
   return (
     <Layout title='trelo'>
-      <Typography variant='h2' color='primary' >Hola Mundo </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 100px)'}}>
+            <CardHeader title="pendientes"/> 
+            <CardContent>
+
+            </CardContent>
+
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 100px)'}} >
+            <CardHeader title="En Progreso"/>            
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 100px)'}} >
+            <CardHeader title="Completadas"/>            
+          </Card>
+        </Grid>
+
+      </Grid>
     </Layout>
   )
 }
